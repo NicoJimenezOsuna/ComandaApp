4e43e2f4ce984dd7912b346864e8b6755967e359
Alberto <Alberto@socialpymesvlc.es>
2020-05-16 16:23:06 +0200

    added router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Redirect, BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Header from './views/Header'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Header/>
+    <Switch>
+      <Route path="/" exact component={App}/>
+      // <Route path="/" component={App}/>
+    </Switch>git status
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
