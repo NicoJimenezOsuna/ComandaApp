9ab3618b3d0a5f74d4b7beb77153a3af6d5e680c
NicoJimenezOsuna <joan@socialpymesvlc.es>
2020-05-16 13:12:26 +0200

    add files

---
+++ src/views/Header.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from "react";
+
+
+const Header = ( ) => {
+
+    return (
+
+        <Fragment>
+            <h1>Trollolo</h1>
+            <h2>Trollolo</h2>
+        </Fragment>
+
+    )
+};
+
+export default Header;
