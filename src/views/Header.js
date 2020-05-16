f2b06cfd74b854987437553df2617155d318676c
NicoJimenezOsuna <joan@socialpymesvlc.es>
2020-05-16 17:10:19 +0200

    res

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,13 +1,57 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 
 
-const Header = ( ) => {
+const Header = () => {
 
     return (
 
         <Fragment>
-            <h1>Trollolo</h1>
-            <h2>Trollolo</h2>
+            <div id="Grupo_17">
+                <div id="Grupo_13">
+                    <div id="Grupo_12">
+                        <svg class="Elipse_1">
+                            <ellipse fill="rgba(255,255,255,1)" stroke="rgba(112,112,112,1)" stroke-width="1px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Elipse_1" rx="42.5" ry="42.5" cx="42.5" cy="42.5">
+                            </ellipse>
+                        </svg>
+                        <svg class="Elipse_2">
+                            <ellipse fill="rgba(255,255,255,1)" stroke="rgba(112,112,112,0.259)" stroke-width="1px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Elipse_2" rx="30" ry="30" cx="30" cy="30">
+                            </ellipse>
+                        </svg>
+                    </div>
+                    <div id="Grupo_11">
+                        <div id="A">
+                            <span>A</span>
+                        </div>
+                        <div id="C">
+                            <span>C</span>
+                        </div>
+                    </div>
+                </div>
+                <div id="Grupo_9">
+                    <div id="powered_by">
+                        <span>powered by</span>
+                    </div>
+                    <img id="socialPymes_Imagotipo" src="socialPymes_Imagotipo.png" srcset="socialPymes_Imagotipo.png 1x, socialPymes_Imagotipo@2x.png 2x"></img>
+		        </div>
+                    <svg class="L_nea_1" viewBox="0 0 720 2">
+                        <path fill="transparent" stroke="rgba(112,112,112,1)" stroke-width="2px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_1" d="M 0 0 L 720 0">
+                        </path>
+                    </svg>
+                    <div id="Grupo_14">
+                        <svg class="L_nea_2" viewBox="0 0 35 3">
+                                <path fill="transparent" stroke="rgba(112,112,112,1)" stroke-width="3px" stroke-linejoin="miter" stroke-linecap="round" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_2" d="M 35 0 L 0 0">
+                                </path>
+                        </svg>
+                        <svg class="L_nea_3" viewBox="0 0 35 3">
+                            <path fill="transparent" stroke="rgba(112,112,112,1)" stroke-width="3px" stroke-linejoin="miter" stroke-linecap="round" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_3" d="M 35 0 L 0 0">
+                            </path>
+                        </svg>
+                        <svg class="L_nea_4" viewBox="0 0 35 3">
+                            <path fill="transparent" stroke="rgba(112,112,112,1)" stroke-width="3px" stroke-linejoin="miter" stroke-linecap="round" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_4" d="M 35 0 L 0 0">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
         </Fragment>
 
     )
