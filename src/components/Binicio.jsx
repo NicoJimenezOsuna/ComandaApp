6c3c9bbe5edccee818820f2e433895859188fd98
Mierizoyluna12 <joan@socialpymesvlc.es>
2020-09-23 20:34:44 +0200

    cambio boton inicio

--- src/components/Binicio.jsx
+++ src/components/Binicio.jsx
@@ -7,8 +7,8 @@ const Binicio = () => {
 
 const styles = {
   boton : {
-    filter: 'drop-shadow(3px 3px 6px rgba(0, 0, 0, 0.161))',
-    WebkitFilter: 'drop-shadow(3px 3px 6px rgba(0, 0, 0, 0.161))',
+    // filter: 'drop-shadow(3px 3px 6px rgba(0, 0, 0, 0.161))',
+    // WebkitFilter: 'drop-shadow(3px 3px 6px rgba(0, 0, 0, 0.161))',
     // position: 'relative',
     overflow: 'visible',
     // width: `313px`,
@@ -17,14 +17,17 @@ const styles = {
     // top: `0px`,
     textAlign: 'center',
     fontFamily: 'Tahoma',
-    // fontSize: `40px`,
+    fontSize: `1.5em`,
     color: 'rgba(112, 112, 112, 1)',
     borderRadius: `50px`,
-    border: `2px rgba(112, 112, 112, 1) solid`,
+    // border: `2px rgba(112, 112, 112, 1) solid`,
     backgroundColor: 'white',
-    width: '150px',
+    // background: 'linear-gradient(145deg, #e6e6e6, #ffffff)',
+    width: '250px',
     padding: '1em',
-    marginBottom: '2em'
+    marginBottom: '2em',
+    // background: '#55b9f3',
+    boxShadow:  '-10px 10px 20px #bfbfbf, 10px -10px 20px #ffffff',
   }
 
 }
